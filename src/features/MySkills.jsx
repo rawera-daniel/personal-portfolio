c7617fcc14e54bdf17d08ed356e5084a9e2a0408
rawera_daniel <rawera.daniel@gmail.com>
2024-01-11 18:07:00 +0300

    refactor: enclose image tag within custom image component

--- src/features/MySkills.jsx
+++ src/features/MySkills.jsx
@@ -1,57 +1,52 @@
 import Heading from "../ui/Heading";
+import Image from "../ui/Image";
 
 function MySkills() {
+  const className = "h-14 w-14";
   return (
     <div className="py-12  text-stone-900">
       <Heading>My skills</Heading>
       <div className=" mx-8 flex flex-wrap items-center  justify-center space-x-2">
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
           alt="HTML"
-          width="55"
-          height="60"
+          className={className}
         />
 
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg"
           alt="CSS"
-          width="55"
-          height="60"
+          className={className}
         />
 
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
           alt="Tailwind CSS"
-          width="70"
-          height="60"
+          className={className}
         />
 
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
           alt="JavaScript"
-          width="50"
-          height="60"
+          className={className}
         />
 
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
           alt="TypeScript"
-          width="50"
-          height="60"
+          className={className}
         />
 
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
           alt="React"
-          width="55"
-          height="60"
+          className={className}
         />
 
-        <img
+        <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
           alt="Github"
-          width="55"
-          height="60"
+          className={className}
         />
       </div>
     </div>
