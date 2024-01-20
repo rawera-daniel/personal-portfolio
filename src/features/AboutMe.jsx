c27b2341938f55cbdc65eb3758615acddd05d570
rawera_daniel <rawera.daniel@gmail.com>
2024-01-20 12:35:52 +0300

    feat: update about me section responsiveness to enhance usability

--- src/features/AboutMe.jsx
+++ src/features/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe() {
   return (
     <div className="py-12 text-stone-900" id="aboutme">
       <Heading>About me</Heading>{" "}
-      <p className="mx-auto w-11/12 text-2xl font-normal leading-9 tracking-wider sm:w-5/6 sm:text-xl sm:leading-8 md:text-lg md:tracking-wide lg:w-4/6 lg:text-base">
+      <p className="mx-auto w-[25%] text-xs font-normal leading-relaxed  tracking-wide sm:w-1/2 sm:text-sm md:w-2/5 md:text-base lg:w-4/6">
         Hello, I’m Rawera 👋, a front-end developer specializing in JavaScript,
         particularly React, based in Addis Ababa, Ethiopia. I'm a driven
         individual who enjoys planning, building, and utilizing my skills to
