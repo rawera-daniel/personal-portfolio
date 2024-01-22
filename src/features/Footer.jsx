b9428b6a2831355c2342a21b2f80765f2f0dcdb0
rawera_daniel <rawera.daniel@gmail.com>
2024-01-23 00:48:05 +0300

    style: enable footer section responsiveness to enhance usability

--- src/features/Footer.jsx
+++ src/features/Footer.jsx
@@ -3,9 +3,9 @@ import SocialLinks from "../ui/SocialLinks";
 function Footer() {
   return (
     <footer className="bg-stone-950">
-      <div className="mx-auto flex max-w-[75%] flex-wrap content-center justify-between px-2 py-5">
+      <div className="mx-auto flex flex-col flex-wrap content-center justify-center space-y-4 px-2 py-5 m:justify-between  m:space-y-6 sm:max-w-[90%] sm:flex-row md:max-w-[80%] lg:max-w-[75%]">
         <SocialLinks />
-        <p className="text-sm font-normal tracking-wider">
+        <p className="text-sm font-normal tracking-wider sm:translate-y-[-12px]">
           Designed & Built by Rawera Daniel
         </p>
       </div>
--- src/ui/SocialLinks.jsx
+++ src/ui/SocialLinks.jsx
@@ -3,21 +3,24 @@ import { FaInstagram, FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
 import { RiWhatsappFill } from "react-icons/ri";
 
 function SocialLinks() {
-  const className = "size-5 hover:fill-stone-400 transition-all duration-500";
+  const className =
+    "sm:size-5 m:size-5 size-5 hover:fill-stone-400 transition-all duration-500";
   return (
-    <div className=" flex flex-wrap  content-center gap-x-3">
-      <ExternalLink href="https://www.linkedin.com/in/rawera-daniel/">
-        <FaLinkedinIn className={className} />
-      </ExternalLink>
-      <ExternalLink href="https://wa.me/251912485944">
-        <RiWhatsappFill className={className} />
-      </ExternalLink>
-      <ExternalLink href="https://twitter.com/Raweradaniel2">
-        <FaXTwitter className={className} />
-      </ExternalLink>
-      <ExternalLink href="https://www.instagram.com/rawera_daniel/">
-        <FaInstagram className={className} />
-      </ExternalLink>
+    <div className="self-center">
+      <div className=" flex flex-wrap  content-center gap-x-4 sm:gap-x-3">
+        <ExternalLink href="https://www.linkedin.com/in/rawera-daniel/">
+          <FaLinkedinIn className={className} />
+        </ExternalLink>
+        <ExternalLink href="https://wa.me/251912485944">
+          <RiWhatsappFill className={className} />
+        </ExternalLink>
+        <ExternalLink href="https://twitter.com/Raweradaniel2">
+          <FaXTwitter className={className} />
+        </ExternalLink>
+        <ExternalLink href="https://www.instagram.com/rawera_daniel/">
+          <FaInstagram className={className} />
+        </ExternalLink>
+      </div>
     </div>
   );
 }
