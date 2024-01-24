5c46e608af7a1a82eb1bb1a95f247ce237493014
rawera_daniel <rawera.daniel@gmail.com>
2024-01-24 10:42:38 +0300

    style: update about me & my skills section responsiveness to enhance usability

--- src/features/AboutMe.jsx
+++ src/features/AboutMe.jsx
@@ -4,13 +4,13 @@ function AboutMe() {
   return (
     <div className="py-12 text-stone-900" id="aboutme">
       <Heading>About me</Heading>{" "}
-      <p className="mx-auto max-w-[25%] text-xs font-normal leading-relaxed  tracking-wide sm:max-w-[50%] sm:text-sm md:max-w-[40%] md:text-base lg:max-w-[66.67%]">
+      <p className="mx-auto max-w-[80%] text-xs font-normal leading-relaxed   tracking-wide m:max-w-[80%] m:text-sm m:leading-relaxed md:max-w-[85%] md:text-base lg:max-w-[75%]">
         Hello, I’m Rawera 👋, a front-end developer specializing in JavaScript,
         particularly React, based in Addis Ababa, Ethiopia. I'm a driven
         individual who enjoys planning, building, and utilizing my skills to
         turn code into a robust product. Attention to detail, problem-solving,
-        and a love for learning from others are key parts of who I am. Outside
-        of coding, you'll find me socializing with friends, engaging in
+        and a love for learning from others are things I like. Outside of
+        coding, you'll find me socializing with friends, engaging in
         conversations with open-minded individuals, or discovering new cafes.
       </p>
     </div>
--- src/features/MySkills.jsx
+++ src/features/MySkills.jsx
@@ -2,11 +2,11 @@ import Heading from "../ui/Heading";
 import Image from "../ui/Image";
 
 function MySkills() {
-  const className = "sm:size-14 size-9";
+  const className = "sm:size-14 m:size-9 size-8";
   return (
     <div className="py-12 text-stone-900" id="myskills">
       <Heading>My skills</Heading>
-      <div className=" mx-8 flex flex-wrap items-center  justify-center space-x-2">
+      <div className=" mx-8 flex flex-wrap items-center justify-center gap-y-4 space-x-3 m:space-x-2">
         <Image
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg"
           alt="HTML"
